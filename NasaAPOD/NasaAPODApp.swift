52cd8a96d297a61a8c5d4688887baf04a849b712
pouyasadri <pouyapaghandeh78@gmail.com>
2024-08-10 16:55:16 +0200

    Initial Commit

---
+++ NasaAPOD/NasaAPODApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaAPODApp.swift
+//  NasaAPOD
+//
+//  Created by Pouya Sadri on 10/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct NasaAPODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
